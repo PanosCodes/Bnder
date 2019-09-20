834c6f2b7aac1086916264e645d9f6f31d5dfa4b
Panos Dalitsouris <panoswise@gmail.com>
2019-09-20 20:03:38 +0300

    Creates Factory class

---
+++ src/Factory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Bender;
+
+class Factory
+{
+    /**
+     * @var
+     */
+    private $class;
+
+    /**
+     * @var array
+     */
+    private $properties;
+
+    /**
+     * Factory constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param string $class
+     * @param array  $properties
+     *
+     * @return Factory
+     */
+    public static function create(string $class, array $properties = []): Factory
+    {
+        $factory = new self();
+        $factory->class = $class;
+        $factory->properties = $properties;
+
+        return $factory;
+    }
+}
