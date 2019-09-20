bcbfdbe3dbfcf788e781f5a9bcc429119b031420
Panos Dalitsouris <panoswise@gmail.com>
2019-09-20 20:12:11 +0300

    Updates tests to support new way to load factories

--- tests/BenderTest.php
+++ tests/BenderTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Bender\Bender;
+use Bender\Factory;
 use Doctrine\ORM\Mapping\Entity;
 use Mocks\SampleEntity;
 use PHPUnit\Framework\TestCase;
@@ -13,9 +14,13 @@ class BenderTest extends TestCase
     {
         $properties = [
             'user' => new Entity(),
-            'email' => 'hello@panos.codes'
+            'email' => 'hello@panos.codes',
         ];
-        $sampleEntity = Bender::load(SampleEntity::class)->create($properties);
+
+        Bender::registerFactory(Factory::create(SampleEntity::class, $properties));
+
+        // Exercise
+        $sampleEntity = Bender::load(SampleEntity::class)->create();
 
         $this->assertInstanceOf(Entity::class, $sampleEntity->getUser());
     }
@@ -24,11 +29,22 @@ class BenderTest extends TestCase
     {
         $properties = [
             'user' => new Entity(),
-            'email' => 'hello@panos.codes'
+            'email' => 'hello@panos.codes',
         ];
-        $entities = Bender::load(SampleEntity::class)->create($properties, 10);
+
+        Bender::registerFactory(Factory::create(SampleEntity::class, $properties));
+        $entities = Bender::load(SampleEntity::class)->create(10);
 
         $this->assertIsArray($entities);
         $this->assertCount(10, $entities);
+        $this->assertInstanceOf(SampleEntity::class, $entities[0]);
+    }
+
+    public function testRegisterFactoryMethod(): void
+    {
+        $factory = Factory::create(SampleEntity::class);
+        $factories = Bender::registerFactory($factory);
+
+        $this->assertCount(1, $factories);
     }
 }
