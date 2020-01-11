9598c8e464ac111ade1bfbb970e78610b7d48a58
Panos Dalitsouris <hello@panos.codes>
2020-01-11 22:59:36 +0200

    Updates SampleEntity with name, id
    and doctrine annotations

--- tests/Mocks/SampleEntity.php
+++ tests/Mocks/SampleEntity.php
@@ -2,10 +2,30 @@
 
 namespace Mocks;
 
-use Doctrine\ORM\Mapping\Entity;
 
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+/**
+ * @Entity
+ * @Table(name="sample_entity")
+ */
 class SampleEntity
 {
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $name;
+
     /**
      * @var Entity
      */
@@ -17,5 +37,33 @@ class SampleEntity
     public function getUser(): Entity
     {
         return $this->user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return SampleEntity
+     */
+    public function setName(string $name): SampleEntity
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
